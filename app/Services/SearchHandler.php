c8102650776d87289be5d325bdb711536490ceac
Sven Luijten <svenluijten1995@gmail.com>
2017-10-02 13:36:54 +0200

    add comment

--- app/Services/SearchHandler.php
+++ app/Services/SearchHandler.php
@@ -31,6 +31,7 @@ class SearchHandler
      */
     public function search($query)
     {
+        // Search query is in format "vendor/package-name"
         if (preg_match('~[a-z0-9]([_.-]?[a-z0-9]+)*\/[a-z0-9]([_.-]?[a-z0-9]+)*~', $query)) {
             return $this->searchWithVendor($query);
         }

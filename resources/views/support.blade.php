7a41fc82f6316411c0a1dadc5cfd6b37d8e79240
Sven Luijten <svenluijten1995@gmail.com>
2020-11-14 14:22:24 +0100

    Update email on support page

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -6,6 +6,6 @@
     <h1>Support</h1>
     <p>
         If you need any help with this app, you can contact me on <a href="//twitter.com/svenluijten">Twitter</a>
-        or shoot me a quick email at svenluijten1995[at]gmail.com.
+        or shoot me a quick email: <a href="mailto:contact@svenluijten.com">contact@svenluijten.com</a>.
     </p>
 @endsection
